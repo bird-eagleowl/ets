e1fc8e4ebec7b87fe9cd441bf7ad9f543e902be2
bird-eagelowl <kockoc.html5@gmail.com>
2024-07-19 22:48:21 +0700

    work_1st 기본 환경 설정 하기

---
+++ kis/__init__.py
@@ -0,0 +1,4 @@
+# ets
+from kis.kisapi import *
+
+__version__ = "0.1.1"
---
+++ test_print.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    print("test print 입니다.")
+    return
+
+if __name__ == '__main__':
+    main()
